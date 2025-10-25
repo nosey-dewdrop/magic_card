6576e278b2742073cb683cc8f5d562d6b1a4fc60
nosey-dewdrop <damummyphus@gmail.com>
2025-10-25 11:36:37 +0300

    pass by val ptr val

--- study_sheet/interrrrnalllpointer.cpp
+++ study_sheet/interrrrnalllpointer.cpp
@@ -2,6 +2,67 @@
 #include <iostream>
 using namespace std;
 
+void incValue(int n) {      // PASS BY VALUE
+    n = n + 5;
+}
+
+void incRef(int& n) {       // PASS BY REFERENCE
+    n = n + 5;
+}
+
+void incPtr(int* n) {       // PASS BY POINTER
+    (*n)++;
+}
+
+
+int main(){
+
+      int x = 10;
+      int* y = &x;
+
+      // cout << x << endl;
+      // cout << &x << endl;
+
+      // cout << y << endl;
+      // cout << *y << endl;
+
+      
+      cout << "\nbefore incValue: " << x << endl;
+      cout << x << endl;
+      cout << y << endl;
+
+      incValue(x);
+
+
+      cout << "\nafter incValue: " << x << endl;
+      cout << x << endl;
+      cout << y << endl;
+
+            
+      cout << "\nbefore incRef: " << x << endl;
+      cout << x << endl;
+      cout << y << endl;
+
+      incRef(x);
+
+
+      cout << "\nafter incRef: " << x << endl;
+      cout << x << endl;
+      cout << y << endl;
+
+      cout << "\nbefore incPtr: " << x << endl;
+      cout << x << endl;
+      cout << y << endl;
+
+      incPtr(y);
+
+      cout << "\nafter incPtr: " << x << endl;
+      cout << x << endl;
+      cout << y << endl;
+
+      return 0;
+}
+
 // int main(){
 //       string name = "damla";
 //       string* name2 = &name;
--- study_sheet/tempCodeRunnerFile.cpp
+++ study_sheet/tempCodeRunnerFile.cpp
@@ -1,20 +1,4 @@
-
-int main(){
-      Player p1(10, "damla");
-
-      string name = p1.getUserName();
-      string* name2 = &name;
-
-      cout<< "\nname is: " << name << endl;
-      cout<< "&name  is: " << &name << endl;
-      cout<< "*&name  is: " << *&name << endl;
-
-      cout<< "\nname2 is: " << name2 << endl;
-      cout<< "*name2  is: " << *name2 << endl;
-
-      cout<< "\n&*name2  is: " << &*name2 << endl;
-      cout<< "*&*name2  is: " << *&*name2 << endl;
-      cout<< "&name2  is: " << &name2 << endl;
-
-      return 0;
-}
+cout << "before incValue: " << x << endl;
+      incValue(x);
+      cout << "after incValue: " << x << endl;
+      cout<< "\n" <<endl;

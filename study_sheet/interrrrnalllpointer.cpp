ac27f35f2baf432a1c9c5c139a5cd5f990fe196e
nosey-dewdrop <damummyphus@gmail.com>
2025-10-25 10:46:51 +0300

    work

---
+++ study_sheet/interrrrnalllpointer.cpp
@@ -0,0 +1,53 @@
+
+#include <iostream>
+using namespace std;
+
+// int main(){
+//       string name = "damla";
+//       string* name2 = &name;
+
+//       cout<< "\nname is: " << name << endl;
+//       cout<< "&name  is: " << &name << endl;
+//       cout<< "*&name  is: " << *&name << endl;
+
+//       cout<< "\nname2 is: " << name2 << endl;
+//       cout<< "&name2  is: " << &name2 << endl;
+//       cout<< "*name2  is: " << *name2 << endl;
+
+//       cout<< "\n&*name2  is: " << &*name2 << endl;
+//       cout<< "*&*name2  is: " << *&*name2 << endl;
+
+//       return 0;
+// }
+
+// int main(){
+
+//       string n1 = "damla";
+//       string n2 = "damla";
+
+//       if(n1 == n2){
+//             cout << "they are the same names"<< endl;
+//             cout << "n1 is:" << n1 << endl;
+//             cout << "n2 is:" << n2 << endl;
+//       }
+//       else{
+//             cout << "not the same"<< endl;
+//             cout << "n1 is:" << n1 << endl;
+//             cout << "n2 is:" << n2 << endl;
+//       }
+
+//       if(&n1 == &n2){
+//             cout << "they are the same adress"<< endl;
+//             cout << "n1 is:" << &n1 << endl;
+//             cout << "n2 is:" << &n2 << endl;
+//       }
+//       else{
+//             cout << "not the same adress "<< endl;
+//             cout << "n1 is: " << &n1 << endl;
+//             cout << "n2 is: " << &n2 << endl;
+//       }
+
+
+//       return 0;
+// }
+
